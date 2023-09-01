9be4b128690738e52dc8012b7e2f09c53f606d4c
Nguyen Hai Ha <appcenter1987@gmail.com>
2023-09-01 13:40:11 +0700

    update

--- native/Application.mk
+++ native/Application.mk
@@ -1,6 +1,6 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 NDK_TOOLCHAIN_VERSION := 4.9
 #APP_STL := stlport_shared
 
